2b879ad7afefd77e5491d0c1eb1e74576de99f19
vilagia <type99hsp@gmail.com>
2023-07-29 14:35:25 +0900

    define some tokens

---
+++ src/entities.rs
@@ -0,0 +1,23 @@
+// Tokens for novel-style text.
+pub enum Token {
+    // Plaintext to be rendered as-is.
+    Text(String),
+    // A comment that should be ignored.
+    Comment(String),
+    // Text to be rendered with additional styles.
+    RichText(String, Attribute),
+    // End of sentence. Includes a string shows the end of sentence(e.g. `.`, `。` or `！`).
+    EndOfSentence(String),
+    // End of paragraph.
+    EndOfParagraph,
+    // End of section such as a scene or a chapter. Includes a string shows the end of section(e.g. `†`).
+    EndOfSection(String),
+    // End of file.
+    EOF,
+}
+
+// Tokens for Rich Text.
+pub enum Attribute {
+    // Ruby(furigana): a small text above the main text.
+    Ruby(String),
+}
